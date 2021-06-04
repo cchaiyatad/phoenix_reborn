4c9ff186e0eaf8a8a8e5a87ce149b0ad6bf9eb79
cchaiyatad <chaiyatad@gmail.com>
2021-06-04 15:26:40 +0700

    Edit: edit worker counter

--- docker/app/app/lib/phoenix_reborn_web/worker/counter.ex
+++ docker/app/app/lib/phoenix_reborn_web/worker/counter.ex
@@ -8,16 +8,16 @@ defmodule PhoenixRebornWeb.Worker.Counter do
 
   @spec start_link(any) :: {:error, any} | {:ok, pid}
   def start_link(_) do
-    Agent.start_link(fn -> 0 end, name: __MODULE__)
+    Agent.start_link(fn -> %{value: 0} end, name: __MODULE__)
   end
 
   @spec value :: integer()
   def value() do
-    Agent.get(__MODULE__, & &1)
+    Agent.get(__MODULE__, fn state -> state.value end)
   end
 
   @spec increment :: :ok
   def increment() do
-    Agent.update(__MODULE__, &(&1 + 1))
+    Agent.update(__MODULE__, fn state -> %{value: state.value + 1} end)
   end
 end
